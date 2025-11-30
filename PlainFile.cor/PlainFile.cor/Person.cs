1da827844be0354b025aed5489fe552eeddfb09f
valentinaalzate <vaev39@gmail.com>
2025-11-29 21:30:54 -0500

    Edit

---
+++ PlainFile.cor/PlainFile.cor/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainFile.cor
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+       
+    }
+
+}
